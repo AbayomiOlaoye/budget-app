554f3e074739a2eca49288686e073b2df6e0c5d2
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-03-11 09:20:15 +0100

    Modify controllers and add custom styles to pages

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,69 +1,28 @@
 class RecordsController < ApplicationController
-  before_action :set_record, only: %i[show edit update destroy]
+  load_and_authorize_resource
 
-  # GET /records or /records.json
   def index
-    @records = Record.all
+    @category = current_category
   end
 
-  # GET /records/1 or /records/1.json
-  def show; end
+  def new; end
 
-  # GET /records/new
-  def new
-    @record = Record.new
-  end
-
-  # GET /records/1/edit
-  def edit; end
-
-  # POST /records or /records.json
   def create
-    @record = Record.new(record_params)
-
-    respond_to do |format|
-      if @record.save
-        format.html { redirect_to record_url(@record), notice: 'Record was successfully created.' }
-        format.json { render :show, status: :created, location: @record }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @record.errors, status: :unprocessable_entity }
-      end
+    @record = current_user.records.new(record_params)
+    if @record.save
+      redirect_to category_records_path(current_category), notice: 'You completed this transaction successfully'
+    else
+      render :new, alert: 'New transaction could not be created.'
     end
   end
 
-  # PATCH/PUT /records/1 or /records/1.json
-  def update
-    respond_to do |format|
-      if @record.update(record_params)
-        format.html { redirect_to record_url(@record), notice: 'Record was successfully updated.' }
-        format.json { render :show, status: :ok, location: @record }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @record.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /records/1 or /records/1.json
-  def destroy
-    @record.destroy
-
-    respond_to do |format|
-      format.html { redirect_to records_url, notice: 'Record was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+  def current_category
+    @categories = current_user.categories.includes(:records).find(params[:category_id])
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_record
-    @record = Record.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
   def record_params
-    params.require(:record).permit(:name, :amount, :user_id)
+    params.require(:record).permit(:name, :amount, category_ids: [])
   end
 end
