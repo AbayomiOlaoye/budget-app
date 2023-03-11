4b9bfbd785600a65ad610ca51494bdba37fcc434
Oluwatoyin Olaoye <mindprepopps@gmail.com>
2023-03-11 14:26:41 +0100

    Fix linter errors

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Record, type: :model do
   subject do
     user = User.new(name: 'Ola Mag', email: 'olamag@mail.com', password: '1234567')
-    Record.new(name: 'Apple MacBook pro', amount: 2000000, user: user)
+    Record.new(name: 'Apple MacBook pro', amount: 2_000_000, user:)
   end
 
   before { subject.save }
